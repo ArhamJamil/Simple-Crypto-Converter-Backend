e36946e5e030b589030aa91f4a56bc8de59c2ab2
ArhamJamil <MUHAMMADARHAMJAMILSABIH@gmail.com>
2024-01-02 01:50:52 +0500

    deployment changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { app } from "./src/app.js";
 
 try {
     app.listen(process.env.PORT, ()=>{
-        console.log(`App running on port: ${process.env.HOST}${process.env.PORT}`)
+        console.log(`App running on port: ${process.env.HOST}`)
     })
 } catch (error) {
     console.log("Error while running app : ", error)
